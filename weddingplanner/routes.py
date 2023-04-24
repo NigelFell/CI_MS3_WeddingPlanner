5c732f9305edc1be84852b502607e19a7199f560
NigelFell <nigelfell@yahoo.co.uk>
2023-04-24 06:20:06 +0000

    feat: added functionality users to add weddings

--- weddingplanner/routes.py
+++ weddingplanner/routes.py
@@ -6,3 +6,19 @@ from weddingplanner.models import Wedding, Task, Supplier
 @app.route("/")
 def home():
     return render_template("weddings.html")
+
+
+@app.route("/add_wedding", methods=["GET", "POST"])
+def add_wedding():
+    if request.method == "POST":
+        wedding = Wedding(
+            wedding_name=request.form.get("wedding_name"),
+            wedding_date=request.form.get("wedding_date"),
+            wedding_town=request.form.get("wedding_town"),
+            wedding_country=request.form.get("wedding_country")
+        )
+        db.session.add(wedding)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_wedding.html")
+
