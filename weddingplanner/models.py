74db6b4b44952887e04baf0100b2c09bea2a1e51
NigelFell <nigelfell@yahoo.co.uk>
2023-04-29 07:10:18 +0000

    fix: changed data type of supplier telephone column

--- weddingplanner/models.py
+++ weddingplanner/models.py
@@ -50,7 +50,7 @@ class Supplier(db.Model):
     # schema for the Supplier model
     id = db.Column(db.Integer, primary_key=True)
     supplier_name = db.Column(db.String(25), unique=True, nullable=False)
-    supplier_telephone = db.Column(db.Integer, nullable=False)
+    supplier_telephone = db.Column(db.String(25), nullable=False)
     supplier_email = db.Column(db.String(25), nullable=False)
     supplier_address = db.Column(db.Text, nullable=False)
     booked = db.Column(db.Boolean, default=False, nullable=False)
