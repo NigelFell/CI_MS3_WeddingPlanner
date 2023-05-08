cfcf119f852412226ee9fa2353860b6c4b195d8e
NigelFell <nigelfell@yahoo.co.uk>
2023-05-08 06:43:01 +0000

    fix: return to wedding_tasks page after task edit or delete

--- weddingplanner/routes.py
+++ weddingplanner/routes.py
@@ -152,12 +152,36 @@ def edit_task(task_id):
     return render_template("edit_task.html", task=task, weddings=weddings)
 
 
+@app.route("/edit_wedding_task/<int:task_id>", methods=["GET", "POST"])
+def edit_wedding_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    wedding = Wedding.query.get_or_404(task.wedding_id)
+    if request.method == "POST":
+        task.task_name = request.form.get("task_name")
+        task.task_description = request.form.get("task_description")
+        task.is_urgent = bool(True if request.form.get("is_urgent") else False)
+        task.due_date = request.form.get("due_date")
+        task.task_completed = bool(
+            True if request.form.get("task_completed") else False)
+        db.session.commit()
+        return redirect(url_for("wedding_tasks", wedding_id=task.wedding_id))
+    return render_template("edit_wedding_task.html", wedding=wedding, task=task)
+
+
 @app.route("/delete_task/<int:task_id>")
 def delete_task(task_id):
     task = Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for("tasks"))
+
+
+@app.route("/delete_wedding_task/<int:task_id>")
+def delete_wedding_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("wedding_tasks", wedding_id=task.wedding_id))
 
 
 @app.route("/add_default_tasks/<int:wedding_id>")
